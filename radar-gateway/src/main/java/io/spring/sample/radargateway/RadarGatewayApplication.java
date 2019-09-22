9aecfcbc14e5a1219d2592b67db7d97b5c1f5e48
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-22 00:08:48 -0400

    Merge add-gateway-module branch: Added gateway module using Spring Cloud Gateway RSocket

---
+++ radar-gateway/src/main/java/io/spring/sample/radargateway/RadarGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.spring.sample.radargateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarGatewayApplication.class, args);
+	}
+
+}
---
+++ radar-gateway/src/test/java/io/spring/sample/radargateway/RadarGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.sample.radargateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
